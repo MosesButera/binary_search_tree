e4f7a1196753b618b63499274af3d3aca789de9b
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2026-06-04 04:35:42 -0600

    feat(node): implement Node class for binary tree structure
    - create standard "Node" class with data, left_child,
    and right_child attributes.
    - Add "attr_accessor" macros to allow public reading
    and writing of node slots.
    - Configure default 'nil' values for child pointers
    in initialization
    - Enable code optimization strings via a frozen string
    literal magic comment

---
+++ lib/node.rb
@@ -0,0 +1,13 @@
+# frozen_string_lateral: true
+
+require_relative 'tree'
+
+class Node
+  attr_accessor :data, :left_child, :right_child
+
+  def initialize(data, left_child = nil, right_child = nil)
+    @data = data
+    @left_child = left_child
+    @right_child = right_child
+  end
+end
